5c6c6c22dc8b8d42bad03e767b89fc561f56b6eb
bhandari123 <bhandari.prakash761@gmail.com>
2023-05-01 22:03:59 -0500

    Assigement07

---
+++ AltaqorAssignments/src/altaqor/assignments/assignment7/car.java
@@ -0,0 +1,27 @@
+package altaqor.assignments.assignment7;
+
+public class car {
+	
+private String carname = "Honda";
+String carmodel = "Toyato xle";
+String caryear = "2023";
+String carcolour = "white";
+
+
+public static void main(String[] args) {
+	
+
+
+String carmake = "Honda";
+String carmodel = "Toyato xle";
+String caryear = "2023";
+String carcolour = "white";
+
+System.out.println("This is my first car");
+System.out.println("carmake : "+carmake);
+System.out.println("carmodel : "+carmodel);
+System.out.println("caryear : "+caryear);
+System.out.println("carcolour : "+carcolour);
+}
+
+}
